4826ffbb666a676bf53ad74d19a28677d3d3d402
Txino2 <sjtuml671@sjtu.edu.cn>
2026-05-06 18:41:05 +0800

    Enhance Vite config with esbuild and server options
    Added esbuild loader configuration for JSX and server settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 禁用 TypeScript 检查
+  esbuild: {
+    loader: 'jsx',
+    include: /\.(jsx?|tsx?)$/,
+    exclude: [],
+  },
+  server: {
+    open: true,
+  }
 })
